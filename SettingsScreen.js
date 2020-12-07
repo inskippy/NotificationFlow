0e660363409b8f005cf75b56bab72ad0f237e76f
montelyu <52260617+montelyu@users.noreply.github.com>
2020-12-06 19:48:12 -0500

    Add files via upload

---
+++ SettingsScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+export default function SettingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+        <h1>Filters</h1>
+        <h2>Time Filter</h2>
+        <h2>App Filter</h2>
+        <p>Enter apps below that you would like to receive notifications for</p>
+        var input = new Array();
+        var x = document.getElementById("myText").value;
+        input.push("Facebook");
+        <h2>Currently selected time ranges for filter</h2>
+        // when receive notification, parse notification array object and check if it exists in the input array containing unwanted apps using a for loop
+ <div class="addStep">
+      <form method="POST" action="#">
+        <label for="newStep">Add a step to personalize this recipe!</label>
+        <input type="text" name="newStep" id="newStep" />
+        <input type="submit" id="addStepBtn" />
+      </form>
+    </div>
+
+
+
+      </View>
+    );
+}
